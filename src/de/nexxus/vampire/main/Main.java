361f6d8fc0b15fa4b3ec9e8abf7bc5c5dc854bd5
DieMaskeLP <DieMaskeLP@users.noreply.github.com>
2019-10-21 16:37:54 +0200

    21.10.2019

--- src/de/nexxus/vampire/main/Main.java
+++ src/de/nexxus/vampire/main/Main.java
@@ -1,6 +1,12 @@
 package de.nexxus.vampire.main;
 
+import de.nexxus.vampire.commands.SetupCommand;
+import de.nexxus.vampire.listener.EventListener;
+import de.nexxus.vampire.listener.PlayerQuitListener;
 import de.nexxus.vampire.manager.Manager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,7 +30,13 @@ public class Main extends JavaPlugin {
     public void onEnable(){
         manager = new Manager();
         plugin = this;
+        registerListener(new EventListener());
+        registerListener(new PlayerQuitListener());
+        getCommand("setup").setExecutor(new SetupCommand());
     }
 
+    private void registerListener(Listener listener){
+        Bukkit.getPluginManager().registerEvents(listener, this);
+    }
 
 }
